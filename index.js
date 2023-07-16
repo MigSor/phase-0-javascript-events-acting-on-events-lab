1ecff17bb36f6dcb3a3894f21e16ed1a80d8b45b
Miguel Soriano <miguel.sor.88@gmail.com>
2023-07-16 14:07:44 +1000

    completed the moveRight and moveLeft of the game

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0";
+
+document.addEventListener("keydown", function (event) {
+  if (event.code === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.code === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerLeft(event) {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const right = parseInt(rightNumbers);
+
+  if (right !== 360) {
+    dodger.style.left = `${right + 10}px`;
+  }
+}
